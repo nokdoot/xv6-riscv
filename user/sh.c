b652c88d13328d87f054d79bc69f88325e6a0a81
nokdoot <nokdoot@naver.com>
2021-11-24 21:57:14 +0900

    test printing array of char, comparing array with literal string

--- user/sh.c
+++ user/sh.c
@@ -157,6 +157,10 @@ main(void)
 
   // Read and run input commands.
   while(getcmd(buf, sizeof(buf)) >= 0){
+    printf("%c", buf[0]);
+    printf("%c", buf[1]);
+    printf("%c", buf[2]);
+    printf("%c", buf[3]);
     if(buf[0] == 'c' && buf[1] == 'd' && buf[2] == ' '){
       // Chdir must be called by the parent, not the child.
       buf[strlen(buf)-1] = 0;  // chop \n
